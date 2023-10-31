65adc5d4969d81c47991e84a5ac1cebcbc869a0c
viljavai <vilja.vaisanen@helsinki.fi>
2023-10-31 15:11:18 +0200

    test coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -9,6 +9,14 @@ class TestVarasto(unittest.TestCase):
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_vaara_tilavuus(self):
+        self.varasto = Varasto(-2)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+    
+    def test_vaara_saldo(self):
+        self.varasto = Varasto(10,-2)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
@@ -30,7 +38,16 @@ class TestVarasto(unittest.TestCase):
         saatu_maara = self.varasto.ota_varastosta(2)
 
         self.assertAlmostEqual(saatu_maara, 2)
-
+    
+    def test_lisataan_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_otetaan_liika(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu_maara = self.varasto.ota_varastosta(6)
+        self.assertAlmostEqual(saatu_maara, 5)
+    
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +55,16 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_ota_maara_negatiivinen(self):
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0.0)
+
+    def test_lisaa_maara_negatiivinen(self):
+        saatu_maara = self.varasto.lisaa_varastoon(-2)
+        self.assertAlmostEqual(saatu_maara, None)
+    
+    def test_str(self):
+        self.varasto = Varasto(10,5)
+        saatu = str(self.varasto)
+        self.assertEqual(saatu, "saldo = 5, vielä tilaa 5")
